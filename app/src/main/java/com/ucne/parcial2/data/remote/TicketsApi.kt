17d020917d311c752c7efc71b5d1cbd329516c89
JoseangelVargaspolaco <jose_vargas7@ucne.edu.do>
2023-03-16 09:17:54 -0400

    Primer commit ( agregando entidad tickets,  interfaz de teprestoApi y interfaz dao)

--- app/src/main/java/com/ucne/parcial2/data/remote/TicketsApi.kt
+++ app/src/main/java/com/ucne/parcial2/data/remote/TicketsApi.kt
@@ -1,9 +1,19 @@
 package com.ucne.parcial2.data.remote
 
-import retrofit2.http.GET
+import com.ucne.parcial2.data.remote.dto.TicketDto
+import retrofit2.Response
+import retrofit2.http.*
 
 
 interface TicketsApi{
+    @GET("/api/tickets")
+    suspend fun getTickets(): List<TicketDto>
+    @POST("/api/Tickets")
+    suspend fun postTickets(@Body ticketDto: TicketDto) : TicketDto
 
+    @PUT("/api/Tickets/{id}")
+    suspend fun putTickets(@Path("id") id: Int, @Body ticketDto: TicketDto): Response<Unit>
 
+    @DELETE("/api/Tickets/{id}")
+    suspend fun deleteTickets(@Path("id") id: Int)
 }
